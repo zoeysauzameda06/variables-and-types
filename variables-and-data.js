26a8877518ef65a4ad1a34effb4f138ef5d9897d
zoeysauzameda06 <zsauzamedavega@student.cnm.edu>
2026-09-12 08:52:01 -0600

    Add files via upload

---
+++ variables-and-data.js
@@ -0,0 +1,28 @@
+/*
+ *Variable, Data Types, and Typing
+ */
+/*
+let add = 1 + 2;
+let sub = 2 - 1;
+let multiply = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "string 1 " + "string 2";
+
+console.log(string);
+*/
+/*
+let obj = {
+    key1: "value 1",
+    key2: 3,
+    obj2: {
+        obj2key1 : "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2key1);
+let wordData;
+
+wordData = "word List"
+ */
